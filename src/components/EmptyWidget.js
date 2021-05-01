b40245f18ad88babe84b05f930059e4c35f2a77a
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2021-05-01 22:50:24 +0530

    Resize Widget

---
+++ src/components/EmptyWidget.js
@@ -0,0 +1,21 @@
+import { Rnd } from 'react-rnd';
+import React from 'react';
+
+
+const EmptyWidget = () => {
+
+    return (
+        <Rnd
+            default={{
+                x: 0,
+                y: 0,
+                width: 320,
+                height: 200,
+            }}
+        >
+            Rnd
+        </Rnd>
+    );
+}
+
+export default EmptyWidget;
